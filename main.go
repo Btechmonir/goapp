ea0de84cbd9543923fe99d3de46a83eca8eb2b55
Btechmonir <hossain1164095@gmail.com>
2023-12-17 17:58:36 +0600

    new commit

--- main.go
+++ main.go
@@ -9,16 +9,6 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.RawQuery)
 	fmt.Fprintf(w, `
-          ##         .
-    ## ## ##        ==
- ## ## ## ## ##    ===
-/"""""""""""""""""\___/ ===
-{                       /  ===-
-\______ O           __/
- \    \         __/
-  \____\_______/
-
-
 What is the Go or Golang programming language?
 
 Go, also called Golang or Go language, is an Open Source programming language that Google developed. Software developers use Go in an array of operating systems and frameworks to develop web applications, cloud and networking services, and other types of software.
